5fa0071e4589f68bb2593c1e7872582d65b88f1b
Vincenz Lachner <vincenz.lachner@gmail.com>
2025-05-09 17:53:29 -0400

    Added number of items to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,7 @@ import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [totalQuantity, setTotalQuantity] = useState(0);
 
     const cart = useSelector(state => state.cart.items)
     const dispatch = useDispatch();
@@ -16,12 +17,16 @@ function ProductList({ onHomeClick }) {
 
     useEffect(() => {
         const map = {};
+        let totalQuantity = 0
         cart.forEach(item => {
             if (item.quantity > 0) {
                 map[item.name] = true;
+                totalQuantity = totalQuantity + item.quantity
             }
         });
         setAddedToCart(map);
+        setTotalQuantity(totalQuantity)
+        console.log(map, totalQuantity)
     }, [cart]);
 
 
@@ -295,7 +300,20 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                { totalQuantity > 0 && <p className="cart_quantity_count">{totalQuantity}</p>}
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="156" height="156" fill="none"></rect>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute">
+                                    </path>
+                                </svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
